9738613fa26e69856a8e3d5d24293f38352ff3aa
Sam Balana <sambalana247@gmail.com>
2017-10-17 21:57:45 -0700

    feat: Alter content for results of peer review
    I got some help from the career center and from my friends Will and Krystian. Thanks guys!

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { Page, Content, Flex, Feature, Header, View, Line, List, PrintOnly, Section, Spacer, Text, Title, Experience } from '../components'
+import { Page, Content, Flex, Feature, Header, Line, List, PrintOnly, Section, Spacer, Text, Title, Experience } from '../components'
 
 export const Home = () => (
     <Page>
@@ -11,7 +11,7 @@ export const Home = () => (
                 <Text>sbalana@uci.edu</Text>
                 <Text>(562) 405-9890</Text>
                 <PrintOnly>
-                    <Text>View this online at sambalana.com</Text>
+                    <Text>View this resume online at sambalana.com</Text>
                 </PrintOnly>
             </Flex>
         </Flex>
@@ -19,47 +19,44 @@ export const Home = () => (
         <Content>
         <Section>
             <Feature>
-                I am a full-stack developer enthusiastic for building practical, comprehendable,
+                I am a full-stack developer enthusiastic about building practical, comprehendible,
                 malleable APIs and applications; sharing my passion through teaching; and constantly
                 learning.
             </Feature>
+        </Section>
+        <Section>
+            <Header>Education</Header>
+            <List
+                items={[
+                    'BS in Computer Science and Engineering at University of California Irvine (2015 - 2019)',
+                    'Relevant classwork: Data Structures, Networks, Organization, Operating Systems, Languages',
+                    'GPA - 3.461',
+                ]}
+            />
+        </Section>
+        <Section>
             <Header>Proficiencies</Header>
             <List
                 items={[
-                    'Large-scale frontend architecture',
+                    'Large-scale frontend web architecture',
                     'React, Redux and ES2017',
                     'Reliable backend architecture',
-                    'Node.js, REST and GraphQL',
+                    'Go, Node.js, REST and GraphQL',
                     'Scalable distributed systems with elastic application deployment',
-                    'Go, Docker and Kubernetes',
+                    'Docker and Kubernetes',
                     'Cross-platform mobile application development',
                     'React Native, iOS and Android',
                 ]}
             />
         </Section>
         <Section>
-            <Header>Open Source Work</Header>
-            <Text>
-                I contribute to the open source community whenever possible; giving back to the tools
-                and libraries I use everyday. Here are a few projects I have contributed to:
-            </Text>
-            <Spacer />
+            <Header>Open Source Contributions</Header>
             <List
                 items={[
-                    'Component-based user interface, facebook/react',
-                    'Redux for React forms, erikras/redux-form',
-                    'Extendable query language, graphql/graphql-js',
-                    'Go file watcher and reloader, tockins/realize',
-                ]}
-            />
-        </Section>
-        <Section>
-            <Header>Education</Header>
-            <List
-                items={[
-                    'Pursuing BS in Computer Science and Engineering at University of California Irvine (2015 - 2019)',
-                    'Relevant classwork: Data Structures, Networks, Organization, Operating Systems, Languages',
-                    'Computer Science at Norco College (2013 - 2015, during high school)',
+                    'Component-based user interface, facebook/react (JavaScript)',
+                    'Redux for React forms, erikras/redux-form (React, Redux)',
+                    'Extendable query language, graphql/graphql-js (Node.js, GraphQL)',
+                    'File watcher and reloader, tockins/realize (Go)',
                 ]}
             />
         </Section>
@@ -67,9 +64,9 @@ export const Home = () => (
             <Header>Projects</Header>
             <List
                 items={[
-                    'Schemed - Personally focused on backend (Go), taught and led small frontend team (React)',
-                    'Tetris Clone - Java',
-                    'boxmodel - JavaScript library for organizing mass amounts of data types',
+                    'Schemed.io is a service to plan meetups; taught and led small frontend team (React); focusing on the backend (Go, MongoDB)',
+                    'Boxmodel is a library for organizing mass amounts of data types for use in frontend development (React, Redux)',
+                    'Tetris Clone is a class final project introducing programming patterns and idioms (Java)',
                 ]}
             />
         </Section>
@@ -81,23 +78,37 @@ export const Home = () => (
                 from='October 2015'
                 to='current'
                 highlights={[
-                    'Ardent Gradebook reduces time to grade student assignments by ~50% (Python, React)',
-                    'Ardent HQ for creation of course cirricula, student and classroom management (Node.js, React, GraphQL)',
-                    'Ardent Solve allows students to complete assignments from an iPad (Node.js, React Native)',
-                    'Led a team to complete the following applications over the summer',
-                    'Ardent Classroom eases attendence and file management (Node.js, React Native, GraphQL)',
-                    'Ardent Family notifies parents of student performance and news (Node.js, React Native, GraphQL)',
+                    'Created Ardent Gradebook, a web app used everyday by teacher assistants that reduces time to grade student assignments by ~50% compared to previous solutions (Python, React)',
+                    'Developing Ardent HQ, a web app for creation of course curricula, student and classroom management; currently hosting 50 courses and 1,000 users (Node.js, RethinkDB, GraphQL, React)',
+                    'Maintaining Ardent Solve, an iPad app allowing students to complete assignments; currently serving 400 students (Node.js, RethinkDB, REST, React Native)',
+                    'Redesigned the entire application stack for microservice-based architecture; increasing API throughput by 300% (Docker, Kubernetes)',
+                    'Created backitup, a utility program for regularly scheduling database backups to Amazon S3; saving the company $30 per month (Go, RethinkDB)',
+                    'Training incoming frontend developers (React, React Native)',
+                    'Leading a team to build the following frontend applications:',
+                    'Supervised Ardent Classroom, an iPad app for staff that eases student attendence and file management (React Native)',
+                    'Supervised Ardent Family, an iOS and Android app that notifies parents of student performance and news (React Native)',
                 ]}
             />
             <Experience
-                position='Teacher / Mentor'
+                position='Teacher & Mentor'
                 company='Ardent Academy for Gifted Youth'
                 from='January 2016'
                 to='current'
                 highlights={[
-                    'Teach middle and high school students how to implement their ideas with React Native',
-                    'Mentor students to guide them through their projects',
-                    'A few student projects: Voluntu (placed 2nd at 2017 Diamond Challenge), AlzheimersGames, ChatterBot, Testaker, iCordis, ASB Vantage, MathFight, HomeAutomation',
+                    'Teaching middle and high school students how to implement their ideas with React Native',
+                    'Mentoring students to guide them through their projects:',
+                    'Voluntu (placed 2nd at 2017 Diamond Challenge), AlzheimersGames, ChatterBot, Testaker, iCordis, ASB Vantage, MathFight, HomeAutomation',
+                ]}
+            />
+            <Experience
+                position='Freelance Web Developer'
+                company='Self-employment'
+                from='January 2017'
+                to='current'
+                highlights={[
+                    'Built website/blog for Archangel Risk Solutions (Ember.js)',
+                    'Designed logo for Archangel Risk Solutions (Illustrator)',
+                    'Developing an online payment portal for Archangel Risk Solutions (Node.js, MongoDB, REST, React, Redux)',
                 ]}
             />
         </Section>
